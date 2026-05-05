777a4d57cbd9b7d5693774bc058edc9b943bb067
almaudud <almadudiakunngoding@gmail.com>
2026-05-05 19:05:28 +0700

    menambahkan string nama

--- staticmember.cpp
+++ staticmember.cpp
@@ -6,4 +6,5 @@ class Mahasiswa { // membuat class mahasiswa
 public:
     static int nim; // deklarasi variabel static yaitu variabel nim
     // deklarasi variabel member
-    
+    int id;
+    string nama;
