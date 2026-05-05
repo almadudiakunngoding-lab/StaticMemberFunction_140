d670edc28c801018dfdaeccf646b02dbf074174e
almaudud <almadudiakunngoding@gmail.com>
2026-05-05 19:20:27 +0700

    menambahkan void angka

--- destructor.cpp
+++ destructor.cpp
@@ -26,3 +26,21 @@ angka::~angka(){ // Destructor
     delete[] arr;
     cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
+
+void angka::cetakData()
+{
+    for (int i = 1; i <= panjang; i++) // loop cetak
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
+void angka::isiData()
+{
+    for (int i = 1; i <= panjang; i++) // loop input
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
